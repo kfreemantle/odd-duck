bbcff7b65f4e1520ddacf12681ccfb17b6b810f7
Kyle Freemantle <kcfreemantle@gmail.com>
2023-02-08 23:23:43 -0800

    completed lab13 except for styling, did a medium amount of refactoring and a huge amount of comments

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 // GLOBAL VARIABLES
 // window to the DOM
 
-// querySelector is what we use to target our document elements.  
+// querySelector is what we use to target our document elements.  You need more practice on proper targeting.
 
 let resultsContainer = document.getElementsByClassName('div2');
 let imgContainer = document.getElementById('div3');  
@@ -43,27 +43,16 @@ function selectRandomProduct() {
   return Math.floor(Math.random() * allProducts.length);  // maximum is inclusive when expressed this way.
 }
 
-// I need a way to check if an array of numbers contains a duplicate.  This was mostly copied from: 
-// https://dev.to/will_devs/javascript-how-to-check-if-an-array-has-duplicate-values-cha
-// with additional help from Michael Treat to get it to work with my code.
-// checkForDuplicates will return a true if the array contains a duplicate value, false if it does not.  
+// saveState and loadState functions for manipulating local storage.  To put into local storage, we have to convert it to a string, store the string in a container, and pack that container into local storage.  It all feels very PSone memory cartridge/emulator save state, so run with that model.
+function saveState() {
+let processedSaveState = JSON.stringify(allProducts);
+localStorage.setItem('allProducts', processedSaveState); 
 
-function checkForDuplicates(array) {
-  let alreadySeen = [];
-  
-  for (let i = 0; i < array.length; i++) {
-    let value = array[i]
-    if (alreadySeen.indexOf(value) !== -1) {
-      return true
-    }
-    alreadySeen.push(value)
-  }
-  return false
 }
+// put loadState here as a static function if it turns out you need it outside of page startup later on..
 
 
 // Constructor for our odd ball products
-
 // name, image src, views, likes/clicks
 
 function OddDuckProduct(name, fileExtension = 'jpg') {
@@ -75,6 +64,12 @@ function OddDuckProduct(name, fileExtension = 'jpg') {
 }
 
 // instances of aaaaaaall our weird Archie McPhee looking products.
+
+// If I wrap this in a function, I can make all of this happen only IF there is nothing to load from local storage.  I want that, and I want to not save anything to local storage unless the game has been completely run, so I should tie saving to local storage to successfully completing the selecting game.
+
+// so I wrap the construction of our objects and the array into if/else statements
+
+if (!localStorage.getItem('allProducts')) {   // if localStorage.getItem does NOT find allProducts, then we create all these products and load them into the allProducts array.  However, if it DOES find allProducts....
 
 let bag = new OddDuckProduct('bag');
 let banana = new OddDuckProduct('banana');
@@ -100,11 +95,19 @@ let wineglass = new OddDuckProduct('wine-glass');
 
 allProducts = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, watercan, wineglass];
 
+} else {    // .....then it's going to do the following code.
+
+  let loadState = localStorage.getItem('allProducts');  // fetch the game's save state from localStorage, and then...
+  allProducts = JSON.parse(loadState);  // ...please do not turn off your system while the loading icon appears, and finally..
+  console.log(allProducts);  // check to make sure it came back properly parsed.
+
+}
+
 
 // within the render function, we need to 
 // 1- select three random items
 // 2 - check if any of them are duplicates of each other
-// 3 - if so, get more items and try again
+// 3 - if so, get more items and try try again
 
 
 function renderProducts() {
@@ -112,7 +115,7 @@ function renderProducts() {
 while (indexArray.length < 6) {
   let randomProducts = selectRandomProduct();  // we're using six here because we're containing enough to account for both the first set of images AND the second set of random images to compare.
   
-  // here we're using the ! operator before our function call to say that we want to return TRUE if the function returns FALSE.  Think of it as a little daemon guy yelling HEY HEY! I WANT NOT THIS, OK?  
+  // here we're using the ! operator before our function call to say that we want to return TRUE if the function returns FALSE.  Think of it as a little discworld daemon guy yelling HEY HEY! I WANT NOT THIS, OK?  
   if (!indexArray.includes(randomProducts)) {
     indexArray.push(randomProducts);
   }
@@ -153,7 +156,10 @@ function renderResults() {
     let li = document.createElement('li');
     li.textContent = `${allProducts[i].name} was selected ${allProducts[i].likes} times out of ${allProducts[i].views} matchups.`;
     results.appendChild(li);
-  }
+  }  // if we put the saveState and renderChart functions to renderResults, we can automagically save the results of our game at the moment we render the chart.  So it's kind of like an auto-save at a checkpoint.  
+  saveState();
+  renderChart();
+
 }
 
 
@@ -174,7 +180,7 @@ if (matchupTotal < matchupAllowed) {
   renderProducts();
 } else { // if the game has hit the matchupAllowed limit, we remove the event listener for clicking the products, and enable ONLY the click listener for the results button.
 imgContainer.removeEventListener('click', handleProductClick);
-// resultsButton.addEventListener('click', renderResults);
+resultsButton.addEventListener('click', renderResults);
 // we're not adding an event listener here anymore, because we're going to automatically render the chart with our results after the game is complete.
 
 renderChart();
@@ -182,17 +188,6 @@ renderChart();
 }
 
 }
-
-
-
-// first iteration of the game is called here.
-
-renderProducts();
-
-// event listener is added and ready for clicks.
-
-imgContainer.addEventListener('click', handleProductClick);
-
 
 
 function renderChart() {
@@ -211,6 +206,8 @@ function renderChart() {
     productLikes.push(allProducts[i].likes);
 
   }
+
+
 
   // here's where we do the chart.js work.  We start with a window into the dom
 
@@ -257,10 +254,17 @@ new Chart(ctx, config);
 }
 
 
+// first iteration of the game is called here.
+
+renderProducts();
+
+// event listener is added and ready for clicks.  
+
+imgContainer.addEventListener('click', handleProductClick);
 
 
 
-
+console.log()
 
 
 
@@ -295,3 +299,24 @@ new Chart(ctx, config);
   // }
 
 // end of renderProducts logic
+
+// duplicate checking code
+
+// I need a way to check if an array of numbers contains a duplicate.  This was mostly copied from: 
+// https://dev.to/will_devs/javascript-how-to-check-if-an-array-has-duplicate-values-cha
+// with additional help from Michael Treat to get it to work with my code.
+// checkForDuplicates will return a true if the array contains a duplicate value, false if it does not.  
+
+// function checkForDuplicates(array) {
+//   let alreadySeen = [];
+  
+//   for (let i = 0; i < array.length; i++) {
+//     let value = array[i]
+//     if (alreadySeen.indexOf(value) !== -1) {
+//       return true
+//     }
+//     alreadySeen.push(value)
+//   }
+//   return false
+// }
+// the checkForDuplicates function is no longer used after some code cleanup, but I like the method so for now I'm keeping it as a visual reminder of a different way to get to the same result.
